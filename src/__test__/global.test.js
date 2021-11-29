4c93aeb45665fe42445770d31de22536900a5d37
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2021-11-29 12:36:14 -0700

    implementando pruebas a promesas

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -26,3 +26,31 @@ test('probar un callback', () => {
 		expect(str).toBe('aloh');
 	});
 });
+
+const reverseString2 = str => {
+	return new Promise((resolve, reject) => {
+		if (!str) {
+			reject(Error('Error'))
+		} else {
+			resolve(str.split("").reverse().join(""))
+		}
+	});
+};
+
+test('Probar una promesa', () => {
+	return reverseString2('hola')
+		.then(string => {
+			expect(string).toBe('aloh');
+		});
+});
+
+test('probar async/await', async () => {
+	const string = await reverseString2('hola');
+	expect(string).toBe('aloh');
+});
+
+afterEach(() => console.log('Despues de cada prueba'));
+afterAll(() => console.log('Despues de todas las pruebas'));
+
+beforeEach(() => console.log('antes de cada prueba'));
+beforeAll(() => console.log('antes de todas las pruebas'));
